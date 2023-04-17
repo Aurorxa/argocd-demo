0bd3ee8777c1841b7ffe5edbc4b3fdad10025934
许大仙 <1900919313@qq.com>
2023-04-17 10:32:36 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 > ArgoCD 的应用 Demo，以 SpringBoot 为例
-> 测试13
--- src/main/java/com/github/web/HelloController.java
+++ src/main/java/com/github/web/HelloController.java
@@ -1,5 +1,14 @@
-package com.github;
+package com.github.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+
 }
